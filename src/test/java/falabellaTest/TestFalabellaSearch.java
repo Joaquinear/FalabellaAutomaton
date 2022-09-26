1b6bd78d0ec1f35795bb95ccf1d0779eb0225ed6
Joaquin Edgar Alcon Ruiz <joaquinear21@gmail.com>
2022-09-26 16:33:11 -0400

    last changes

--- src/test/java/falabellaTest/TestFalabellaSearch.java
+++ src/test/java/falabellaTest/TestFalabellaSearch.java
@@ -1,10 +1,14 @@
 package falabellaTest;
 
+import actions.GetList;
+import actions.WaitUntilElement;
 import base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import task.falabella.TfalabellaIndex;
 import task.google.TGoogleClick;
 import task.google.TGoogleSearch;
+import ui.falabella.UiFalabellaResult;
 
 public class TestFalabellaSearch extends BaseTest {
     @Test
@@ -12,5 +16,10 @@ public class TestFalabellaSearch extends BaseTest {
         TGoogleSearch.InputSearch(driver,"Falabella colombia");
         TGoogleClick.firstResult(driver);
         TfalabellaIndex.search(driver,"Consola ps5");
+        //Assert.assertEquals("Consola Sony PS5 Digital 825GB'", WaitUntilElement.isPresent(driver,GetList.elements(driver,UiFalabellaResult.itemSelect).get(0));
+    }
+    @Test
+    public void falabellaResultElement(){
+
     }
 }
