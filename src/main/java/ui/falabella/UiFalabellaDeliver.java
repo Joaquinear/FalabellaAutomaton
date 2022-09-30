eb95b97ee25457e4281872acf19e7986623ef848
Joaquin Edgar Alcon Ruiz <joaquinear21@gmail.com>
2022-09-30 14:31:24 -0400

    falabella pay item UI

---
+++ src/main/java/ui/falabella/UiFalabellaDeliver.java
@@ -0,0 +1,17 @@
+package ui.falabella;
+
+import org.openqa.selenium.By;
+
+public class UiFalabellaDeliver {
+    public static By departament = By.id("region");
+    public static By city = By.id("ciudad");
+    public static By neighborhood = By.id("comuna");
+    public static By BtnContinue = By.xpath("(//button[text()='Continuar'])[2]");
+    //---------------Despues del continuar---------------------------------------------------//
+    public static By address = By.id("address");
+    public static By Departament = By.id("departmentNumber");
+    public static By btnEnterAddress = By.xpath("//button[@name='useAddress']");
+    public static By BtnContinuarGoToPay = By.xpath("//button[text()='Continuar']");
+    public static By TexPayLastStep = By.xpath("//span[text()='Pago']");
+
+}
